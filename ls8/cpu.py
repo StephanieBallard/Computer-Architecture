e7f368e1b5a6223e518a90d863b2f664e2ed9adc
Stephanie <user.name>
2020-10-14 19:05:53 -0700

    tuesday mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,10 +2,6 @@
 
 import sys
 
-HLT = 0b00000001   # 1
-LDI = 0b10000010   # 130
-PRN = 0b01000111   # 71
-
 class CPU:
     """Main CPU class."""
 
@@ -25,34 +21,64 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
+        # Read program data
         address = 0
+        if len(sys.argv) != 2:
+            print("usage: comp.py progname")
+            sys.exit(1)
+
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    line = line.strip()
+
+                    if line == '' or line[0] == "#":
+                        continue
+
+                    try:
+                        str_value = line.split("#")[0]
+                        value = int(str_value, 2)
+
+                    except ValueError:
+                        print(f"Invalid number: {str_value}")
+                        sys.exit(1)
+
+                    self.ram[address] = value
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"File not found: {sys.argv[1]}")
+            sys.exit(2)
+
+        # address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.register[reg_a] *= self.register[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+        
 
     def trace(self):
         """
@@ -61,22 +87,27 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
+            self.PC,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.PC),
+            self.ram_read(self.PC + 1),
+            self.ram_read(self.PC + 2)
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
     def run(self):
         """Run the CPU."""
         running = True
+
+        HLT = 0b00000001   # 1
+        LDI = 0b10000010   # 130
+        PRN = 0b01000111   # 71
+        MUL = 0b10100010
 
         while running:
             ir = self.ram_read(self.PC)
@@ -88,11 +119,16 @@ class CPU:
 
             elif ir == LDI:
                 self.register[operand_a] = operand_b
+                self.PC += 3
 
             elif ir == PRN:
                 print(self.register[operand_a])
+                self.PC += 2
+            
+            elif ir == MUL:
+                self.alu("MUL", operand_a,operand_b)
+                self.PC += 3
 
-cpu = CPU
 
 # Monday:
 # - [X] Inventory what is here
